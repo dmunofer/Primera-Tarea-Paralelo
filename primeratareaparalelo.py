1b36ba6ec9efec7d32d32885860fdb848790588d
dmunofer <diego.fmz@hotmail.com>
2023-02-13 16:55:21 +0100

    cambios

--- primeratareaparalelo.py
+++ primeratareaparalelo.py
@@ -6,7 +6,7 @@ def scrape(url):
     print("starting", url) # Imprime un mensaje indicando que se está iniciando el raspado de la URL
     duration = round(random.random(),3) # Genera un número aleatorio entre 0 y 1, lo redondea a 3 decimales y lo asigna a la variable duration
     sleep(duration) # Duerme el proceso durante la cantidad de segundos especificada en duration
-    print("finished", url, "time taken:", duration, "seconds")
+    print("finished", url, "time taken:", duration, "seconds")  # Imprime un mensaje indicando que se terminó el raspado de la URL y el tiempo que tomó
     return url, duration
 urls = ["a.com", "b.com", "c.com", "d.com"]
 if __name__ == "__main__":
