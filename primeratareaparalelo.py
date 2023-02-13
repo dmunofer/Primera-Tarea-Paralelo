86be36f9d92ea534c6d047e04b4de7773a949e7c
dmunofer <diego.fmz@hotmail.com>
2023-02-13 16:51:37 +0100

    cambios

---
+++ primeratareaparalelo.py
@@ -0,0 +1,17 @@
+from multiprocessing import Pool  # Importa la clase Pool del módulo multiprocessing
+from time import sleep
+import random
+def scrape(url):
+    print("starting", url)
+    duration = round(random.random(),3)
+    sleep(duration)
+    print("finished", url, "time taken:", duration, "seconds")
+    return url, duration
+urls = ["a.com", "b.com", "c.com", "d.com"]
+if __name__ == "__main__":
+    pool = Pool(processes=4)
+    data = pool.map(scrape, urls)
+    pool.close()    
+    print()
+    for row in data:
+        print(row)
